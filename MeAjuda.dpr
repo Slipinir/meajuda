7bc369004208662a16c8f7e9fac26c97d56b9af1
Luiz Carlos <luiz@alternativasistemas.com.br>
2020-12-03 09:47:50 -0300

    Alterações no dpr

--- MeAjuda.dpr
+++ MeAjuda.dpr
@@ -6,16 +6,13 @@ uses
   untLogin in 'untLogin.pas' {Form1},
   untMenu in 'untMenu.pas' {Form2},
   untFormulario in 'untFormulario.pas' {Form3},
-  untMap in 'untMap.pas' {Form4};
+  untMap in 'untMap.pas' {FormMapa};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.FormFactor.Orientations := [TFormOrientation.Portrait];
-//  Application.CreateForm(TForm1, Form1);
-//  Application.CreateForm(TForm2, Form2);
-//  Application.CreateForm(TForm3, Form3);
-  Application.CreateForm(TForm4, Form4);
+  Application.CreateForm(TFormMapa, FormMapa);
   Application.Run;
 end.
